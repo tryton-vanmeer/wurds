7586a3f75e1f7964cbcd78b3aec908699a1a28ba
Tryton Van Meer <hello@trytonvanmeer.dev>
2022-07-22 21:14:19 -0400

    use commit sha as version.

--- build.rs
+++ build.rs
@@ -1,11 +1,9 @@
 use anyhow::Result;
-use vergen::{vergen, Config};
-use vergen::{ShaKind, TimestampKind};
+use vergen::{vergen, Config, ShaKind};
 
 fn main() -> Result<()> {
     let mut vergen_config = Config::default();
     *vergen_config.git_mut().sha_kind_mut() = ShaKind::Short;
-    *vergen_config.git_mut().commit_timestamp_kind_mut() = TimestampKind::DateOnly;
 
     vergen(vergen_config)
 }
